5d58a57c1324d6773f0792fabbec855bb9d32b82
Pavel Kostadinov <pavel.kostadinov@secretsales.com>
2024-08-05 16:18:28 +0300

    Prerender fixes + file cache

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({
+  port: process.env.PRERENDER_PORT || 3000,
+  enableServiceWorker: true,
+  softIterations: 30,
+  pageDoneCheckInterval: 100,
+  waitAfterLastRequest: 500,
+  pageLoadTimeout: 40000,
+  logRequests: true,
+  captureConsoleLog: true,
+  logger: true,
+});
 
-server.use(prerender.sendPrerenderHeader());
+// server.use(require('prerender-memory-cache'));
+server.use(prerender.prerenderCacheFileSystem());
+
 server.use(prerender.browserForceRestart());
-// server.use(prerender.blockResources());
-server.use(prerender.addMetaTags());
+
+server.use(prerender.blockResources());
+server.use(prerender.sendPrerenderHeader());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
